bdb064a4cafc9446083f07b111d99130b86cace2
juriakajurek <juriakajurek@github.com>
2021-02-08 22:40:14 +0100

    First commit

---
+++ backend/refit/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '261b3695db315d913ee76781d6f22f48'),
+    },
+  },
+});
