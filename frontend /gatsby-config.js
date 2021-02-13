fe2643cb63b63ff3cdbb4726e1e939cbb15c64b8
juriakajurek <juriakajurek@github.com>
2021-02-13 15:36:48 +0100

    safe backup

--- frontend /gatsby-config.js
+++ frontend /gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       options: {
         apiURL: "http://localhost:1337",
         queryLimit: 1000,
-        contentTypes: [`services`],
+        contentTypes: [`services`, `rooms`, `questionnaires`],
       },
     },
   ],
